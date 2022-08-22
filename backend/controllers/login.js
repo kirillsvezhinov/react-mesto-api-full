2330394f45af2d425206f16510378097be2f1aef
UserGitHub37 <bum37@mail.ru>
2022-08-23 01:47:06 +0300

    chore: add option SameSite: 'none'

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -13,6 +13,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 7 * 24 * 60 * 60 * 1000, // 7 дней
           httpOnly: true,
+          SameSite: 'none',
         })
         .send({
           message: 'Успешная авторизация',
